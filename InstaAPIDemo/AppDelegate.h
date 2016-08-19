78c5ee276f04db5e8e47544f8559a4c1789967b9
Ryan Archer <rlarcher@andrew.cmu.edu>
2016-08-19 14:43:40 -0400

    Initial Commit

---
+++ InstaAPIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaAPIDemo
+//
+//  Created by Ryan Archer on 8/19/16.
+//  Copyright © 2016 RyanArcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaAPIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaAPIDemo
+//
+//  Created by Ryan Archer on 8/19/16.
+//  Copyright © 2016 RyanArcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
